b2578b8f0277fe59c3ab22a19e49e1736060a2a8
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2023-12-06 16:25:38 -0600

    added var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-fetch ('https://api.openweathermap.org/data/2.5/forecast?q=UnitedStates&limit=5&appid&appid=db0dc706eb3beb6a4611c862293b870b',{
+var searchBtn = document.querySelector('.city-search')
+var cityText = document.querySelector('.city-text')
+var APIkey = 'db0dc706eb3beb6a4611c862293b870b'
 
+var Coordinates = {
+    
+}
+
+
+
+searchBtn.addEventListener('click', Coordinates)
+
+
+fetch ('https://api.openweathermap.org/data/2.5/weather?q=miami&appid=db0dc706eb3beb6a4611c862293b870b',{
 })
 .then(function (response) {
     return response.json();
