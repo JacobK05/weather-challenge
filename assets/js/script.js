b62d9250e6c4d2c026c0f5577286da46b6935471
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2023-12-08 12:33:12 -0600

    got the weather api to get the city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
 var searchBtn = document.querySelector('.city-search')
-var cityText = document.querySelector('.city-text')
+var cityText = document.querySelector('.city-text');
 var APIkey = 'db0dc706eb3beb6a4611c862293b870b'
 
-var Coordinates = {
-    
-}
 
 
 
-searchBtn.addEventListener('click', Coordinates)
-
-
-fetch ('https://api.openweathermap.org/data/2.5/weather?q=miami&appid=db0dc706eb3beb6a4611c862293b870b',{
+searchBtn.addEventListener('click', function(){
+var cityName = cityText.value
+var weatherApi =`http://api.openweathermap.org/geo/1.0/direct?q=${cityText},&appid=${APIkey}`
 })
+
+fetch ()
 .then(function (response) {
     return response.json();
   })
